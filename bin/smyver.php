296320d1cad990bc6b1f9c6d49f795e9bc4deae3
Joe Green <joe.green@smrtr.co.uk>
2016-07-20 13:48:03 +0100

    Require composer autoloader in smyver.php

--- bin/smyver.php
+++ bin/smyver.php
@@ -5,6 +5,8 @@
  * @author Joe Green <joe.green@smrtr.co.uk>
  */
 
+require_once realpath(dirname(__FILE__).'/../../../autoload.php');
+
 $app = new \Symfony\Component\Console\Application('smyver');
 
 $app->add(new \Smrtr\MysqlVersionControl\Command\Up);
