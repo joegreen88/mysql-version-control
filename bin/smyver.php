e27cb7bdf4f22f1b6050d1f81b99c7ad89ccdf27
Joe Green <joe.green@smrtr.co.uk>
2016-07-21 10:32:45 +0100

    Add status command to CLI app

--- bin/smyver.php
+++ bin/smyver.php
@@ -9,6 +9,7 @@ require_once realpath(dirname(__FILE__).'/../../../autoload.php');
 
 $app = new \Symfony\Component\Console\Application('smyver');
 
+$app->add(new \Smrtr\MysqlVersionControl\Command\Status);
 $app->add(new \Smrtr\MysqlVersionControl\Command\Up);
 $app->add(new \Smrtr\MysqlVersionControl\Command\Teardown);
 
